db9b958b583aab050cd1bc3ed461fce1c56cfb28
tong <tong@disktree.net>
2021-05-18 12:52:35 +0200

    Update example

--- example/App.hx
+++ example/App.hx
@@ -1,22 +1,18 @@
 
 import js.npm.sqlite3.Database;
 
-class App {
-
-	static function main() {
-		var db = new Database( Database.MEMORY );
-		db.serialize( function() {
-			db.run( "CREATE TABLE lorem (info TEXT)" );
-			var stmt = db.prepare( "INSERT INTO lorem VALUES (?)" );
-			for( i in 0...10 ) {
-				stmt.run( "Ipsum "+i );
-			}
-			stmt.finalize();
-			db.each( "SELECT rowid AS id, info FROM lorem", function(err,row) {
-				trace( row.id + ": " + row.info );
-			});
+function main() {
+	var db = new Database( Database.MEMORY );
+	db.serialize( () -> {
+		db.run( "CREATE TABLE lorem (info TEXT)" );
+		var stmt = db.prepare( "INSERT INTO lorem VALUES (?)" );
+		for( i in 0...10 ) {
+			stmt.run( "Ipsum "+i );
+		}
+		stmt.finalize();
+		db.each( "SELECT rowid AS id, info FROM lorem", (err,row) -> {
+			trace( row.id + ": " + row.info );
 		});
-		db.close();
-	}
-
+	});
+	db.close();
 }
